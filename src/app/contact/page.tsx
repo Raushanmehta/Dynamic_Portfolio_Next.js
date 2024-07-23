f50d8ea46f778fe7e6813cc5aacff1fbd6c64b84
Raushan Mehta <raushanmehta2184@gmail.com>
2024-07-23 12:33:53 +0530

    email function add

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,28 +2,66 @@
 import React from 'react';
 
 const Contact: React.FC = () => {
+  const [result, setResult] = React.useState("Submit");
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+
+  const sendMessage = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (isSubmitting) return; // Prevent multiple submissions
+    setIsSubmitting(true);
+    setResult("Sending...");
+    
+    const formData = new FormData(event.currentTarget);
+    formData.append("access_key", "86bbbc1b-2f0d-437b-938f-526cefcd6c30");
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.currentTarget.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    } catch (error) {
+      console.error("Error submitting form", error);
+      setResult("submitting succesfully.");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <div className="bg-black/[0.96]">
       {/* Title */}
-      <div className="flex flex-col py-10 items-center justify-center text-center">
-        <h2 className="text-white font-semibold text-2xl md:text-4xl md:leading-tight">Contact us</h2>
-        <p className="mt-1 text-neutral-400">Whatever your goal - we will get you there.</p>
+      <div className="flex flex-col py-8 items-center justify-center text-center">
+        <p className='text-neutral-400'>Contact with me</p>
+        <h2 className="text-white font-semibold text-2xl md:text-4xl md:leading-tight">Get in touch</h2>
+        <p className="mt-8 text-neutral-400">I'd love to hear from you! if you have any question, comments or feedback, please use the form below.</p>
       </div>
       {/* End Title */}
-      <div className="md:mx-24 px-4 lg:py-20">
+      <div className="md:mx-24 px-4 md:py-10">
         {/* Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 lg:gap-x-16">
           <div className="md:order-2 border-b border-neutral-800 pb-10 mb-10 md:border-b-0 md:pb-0 md:mb-0">
-            <form>
+            <form onSubmit={sendMessage}>
               <div className="space-y-4">
                 {/* Input */}
                 <div className="relative">
                   <input
                     type="text"
                     id="input-name"
+                    name="name"
                     className="peer p-4 block w-full bg-black border rounded-lg text-sm text-white placeholder:text-transparent focus:outline-none focus:ring-10 disabled:opacity-50 disabled:pointer-events-none
                     focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2"
                     placeholder="Name"
+                    required
                   />
                   <label
                     htmlFor="input-name"
@@ -40,9 +78,11 @@ const Contact: React.FC = () => {
                   <input
                     type="email"
                     id="input-email"
+                    name="email"
                     className="peer p-4 block w-full bg-black border rounded-lg text-sm text-white placeholder:text-transparent focus:outline-none focus:ring-0 disabled:opacity-50 disabled:pointer-events-none
                     focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2"
                     placeholder="Email"
+                    required
                   />
                   <label
                     htmlFor="input-email"
@@ -54,15 +94,16 @@ const Contact: React.FC = () => {
                 </div>
                 {/* End Input */}
 
-
                 {/* Input */}
                 <div className="relative">
                   <input
                     type="text"
                     id="input-phone"
+                    name="phone"
                     className="peer p-4 block w-full bg-black border rounded-lg text-sm text-white placeholder:text-transparent focus:outline-none focus:ring-0 disabled:opacity-50 disabled:pointer-events-none
                     focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2"
                     placeholder="Phone"
+                    required
                   />
                   <label
                     htmlFor="input-phone"
@@ -78,9 +119,11 @@ const Contact: React.FC = () => {
                 <div className="relative">
                   <textarea
                     id="message"
+                    name="textarea"
                     className="peer p-4 block w-full bg-black border rounded-lg text-sm text-white placeholder:text-transparent focus:outline-none focus:ring-0 disabled:opacity-50 disabled:pointer-events-none
                     focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2"
                     placeholder="This is a textarea placeholder"
+                    required
                   />
                   <label
                     htmlFor="message"
@@ -97,11 +140,11 @@ const Contact: React.FC = () => {
                 <p className="text-xs text-neutral-500">All fields are required</p>
               </div>
               <button
-    type="submit"
-    className="mt-6 w-full bg-purple-600 text-white px-4 py-2 rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-800 focus:ring-opacity-50 duration-100"
-  >
-    Submit
-  </button>
+                type="submit"
+                className="mt-6 w-full bg-purple-600 text-white px-4 py-2 rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-800 focus:ring-opacity-50 duration-100"
+              >
+                {result}
+              </button>
             </form>
           </div>
           {/* End Col */}
@@ -126,9 +169,8 @@ const Contact: React.FC = () => {
               </svg>
               <div className="grow">
                 <h4 className="text-white font-semibold">Our address:</h4>
-
                 <address className="mt-1 text-neutral-400 text-sm not-italic">
-                  Gujrat ,Ahmdhabad
+                  Gujrat, Ahmedabad
                   <br />
                   India
                 </address>
@@ -155,9 +197,8 @@ const Contact: React.FC = () => {
               </svg>
               <div className="grow">
                 <h4 className="text-white font-semibold">Email us:</h4>
-
-                <a className="mt-1 text-neutral-400 text-sm" href="#raushanmehta2184@gmail.com" target="_blank" rel="noopener noreferrer">
-                  raushanmeta2184@gmail.com 
+                <a className="mt-1 text-neutral-400 text-sm" href="mailto:raushanmehta2184@gmail.com" target="_blank" rel="noopener noreferrer">
+                  raushanmehta2184@gmail.com
                 </a>
               </div>
             </div>
@@ -181,11 +222,10 @@ const Contact: React.FC = () => {
                 <path d="M11.6 16.8a3 3 0 1 1-5.8-1.6" />
               </svg>
               <div className="grow">
-                <h4 className="text-white font-semibold">I am looking for Job </h4>
+                <h4 className="text-white font-semibold">I am looking for Job</h4>
                 <p className="mt-1 text-neutral-400">
                   We're thrilled to announce that we're expanding our team and looking for talented individuals like you to join us.
                 </p>
-                
               </div>
             </div>
             {/* End Item */}
